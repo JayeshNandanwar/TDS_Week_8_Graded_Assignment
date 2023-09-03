4958d964fb223b92205c8df1b919dadde6b91d0d
JayeshNandanwar <131257587+JayeshNandanwar@users.noreply.github.com>
2023-09-03 20:40:02 +0530

    Update app.py
    Added Smallest Number

--- app.py
+++ app.py
@@ -28,7 +28,23 @@ def find_largest(data):
      largest_num = data['Second_Num']
   return largest_num
 
+def find_smallest(data):
+  smallest_num=None
+  if data['First_Num'] <= data['Second_Num']:
+    if data['First_Num'] <= data['Third_Num']:
+      smallest_num = data['First_Num']
+    else:
+      smallest_num = data['Third_Num']
+  elif  data['Third_Num'] <= data['Second_Num']:
+    smallest_num = data['Third_Num']
+  else:
+     smallest_num = data['Second_Num']
+  return smallest_num
+
+  
 data = user_input_features()
+Smallest_Num = find_smallest(data)
 Largest_Num = find_largest(data)
 
+st.subheader("Smallest Number is : " + str(Smallest_Num))
 st.subheader("Largest Number is : " + str(Largest_Num))
